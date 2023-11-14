b3669cc5da80c925952786a0a2f714c6ad923647
DariaZharkova <139045140+DariaZharkova@users.noreply.github.com>
2023-11-15 01:51:28 +0200

    add task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector(".gallery");
+
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+container.addEventListener("click", onClick);
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+   <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+    `
+    )
+    .join("");
+}
+
+function onClick(evt) {
+  evt.preventDefault();
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+        <img
+        src="${evt.target.dataset.source}" 
+        alt="${evt.target.alt}"
+        />
+    `);
+
+  instance.show();
+}
